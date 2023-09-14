7e5aad470519c2b97196a2acf867ebd9fb05113d
boimolongs <boipelo.molongwane@gmail.com>
2023-09-14 16:30:24 -0700

    add #0

---
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -0,0 +1,11 @@
+#include "main.h"
+
+
+int _isupper(int c) {
+    if (c >= 'A' && c <= 'Z') {
+        return 1; // It is uppercase
+    } else {
+        return 0; // It is not uppercase
+    }
+}
+
