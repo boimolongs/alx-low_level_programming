dee4405fd084888ed2930a1893f25b049ac8a141
boimolongs <boipelo.molongwane@gmail.com>
2023-09-14 19:33:29 -0700

    fix

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -1,60 +1,36 @@
 #include "main.h"
+#include <stdio.h>
 
 /**
- * _putchar - Custom function to print a character
- * @c: The character to be printed
- *
- * Return: Number of characters printed
+ * main - prints the numbers from 1 to 100, followed by a new line
+ * but for multiples of three prints Fizz instead of the number
+ * and for the multiples of five prints Buzz
+ * Return: Always 0 (Success)
  */
+int main(void)
+{
+	int i;
 
-/**
- * custom_print - Prints a string
- * @str: The string to print
- */
-void custom_print(const char * str) {
-    while ( * str) {
-        _putchar( * str);
-        str++;
-    }
-}
+	for (i = 1; i <= 100; i++)
+	{
+		if (i % 3 == 0 && i % 5 != 0)
+		{
+			printf(" Fizz");
+		} else if (i % 5 == 0 && i % 3 != 0)
+		{
+			printf(" Buzz");
+		} else if (i % 3 == 0 && i % 5 == 0)
+		{
+			printf(" FizzBuzz");
+		} else if (i == 1)
+		{
+			printf("%d", i);
+		} else
+		{
+			printf(" %d", i);
+		}
+	}
+	printf("\n");
 
-/**
- * custom_print_num - Prints a number
- * @num: The number to print
- */
-void custom_print_num(int num) {
-    if (num == 0) {
-        _putchar('0');
-        return;
-    }
-
-    if (num / 10 != 0) {
-        custom_print_num(num / 10);
-    }
-
-    _putchar('0' + num % 10);
-}
-
-int main(void) {
-    int i;
-
-    for (i = 1; i <= 100; i++) {
-        if (i % 3 == 0 && i % 5 == 0) {
-            custom_print("FizzBuzz");
-        } else if (i % 3 == 0) {
-            custom_print("Fizz");
-        } else if (i % 5 == 0) {
-            custom_print("Buzz");
-        } else {
-            custom_print_num(i);
-        }
-
-        if (i < 100) {
-            custom_print(" ");
-        } else {
-            custom_print("\n");
-        }
-    }
-
-    return (0);
+	return (0);
 }
