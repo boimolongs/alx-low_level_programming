6775f0220f7ee9fead8da57afd12f8465b518663
boimolongs <boipelo.molongwane@gmail.com>
2023-09-14 17:21:04 -0700

    fixed styling

--- 0x04-more_functions_nested_loops/0-isupper.c
+++ 0x04-more_functions_nested_loops/0-isupper.c
@@ -2,18 +2,16 @@
 
 
 /**
- * _isupper - checks for uppercase character
- * c -  the character to be checked
+ * _isupper - Checks if a character is uppercase
+ * @c: The character to be checked
+ *
  * Return: 1 if c is uppercase, 0 otherwise
  */
-
 int _isupper(int c)
 {
-    if (c >= 'A' && c <= 'Z') 
-        return (1); // It is uppercase
-    
-    else 
-        return (0); // It is not uppercase
-    
+	if (c >= 'A' && c <= 'Z')
+		return (1);
+	else
+		return (0);
 }
 
