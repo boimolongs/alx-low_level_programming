3af836eea03c26e251b614e9086b10778e593b47
boimolongs <boipelo.molongwane@gmail.com>
2023-09-21 19:37:35 -0700

    format

--- 0x06-pointers_arrays_strings/0-strcat.c
+++ 0x06-pointers_arrays_strings/0-strcat.c
@@ -9,23 +9,20 @@
  */
 char *_strcat(char *dest, char *src)
 {
-    int dest_len = 0;
-    int i = 0;
+	int dest_len = 0;
+	int i = 0;
 
-    while (dest[dest_len] != '\0')
-    {
-	    dest_len++;
-    }
-
-    while (src[i] != '\0')
-    {
-	    dest[dest_len] = src[i];
-	    dest_len++;
-	    i++;
-    }
-
-    dest[dest_len] = '\0';
-
-    return (dest);
+	while (dest[dest_len] != '\0')
+	{
+		dest_len++;
+	}
+	while (src[i] != '\0')
+	{
+		dest[dest_len] = src[i];
+		dest_len++;
+		i++;
+	}
+	dest[dest_len] = '\0';
+	return (dest);
 }
 
