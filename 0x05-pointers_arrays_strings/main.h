4477d556a7091234fcd1d6dad27fbbdd94aa8176
boimolongs <boipelo.molongwane@gmail.com>
2023-09-25 14:31:04 -0700

    update main.h

--- 0x05-pointers_arrays_strings/main.h
+++ 0x05-pointers_arrays_strings/main.h
@@ -1,9 +1,15 @@
 #ifndef MAIN_H
 #define MAIN_H
 
-/* Function prototype for reset_to_98 */
+/* Function prototypes*/
+int _putchar(char c);
 void reset_to_98(int *n);
 void swap_int(int *a, int *b);
+int _strlen(char *s);
+void _puts(char *str);
+void print_rev(char *s);
+void rev_string(char *s);
+void puts2(char *str);
 
 #endif /* MAIN_H */
 
